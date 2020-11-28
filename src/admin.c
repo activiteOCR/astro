518be218e1ce46a037f4bced32d7b26285ee196b
activiteOCR <julien.terrier3@wanadoo.fr>
2020-11-28 10:10:28 -0500

    Add functions and corrige issue

--- src/admin.c
+++ src/admin.c
@@ -9,9 +9,9 @@
 void listing_client(FILE *rep)
 {
 	CLIENT p;
-	
+
 	rep=fopen("bdd_client.txt","r");
-	
+
 	if(rep != NULL)
     {
 		fseek(rep, 0, SEEK_SET);
@@ -26,9 +26,9 @@ void listing_client(FILE *rep)
 void saisir_nouveau_client(FILE *rep)
 {
     CLIENT p;
-	
+
 	rep=fopen("bdd_client.txt","a+");
-	
+
     if(rep != NULL)
     {
         printf("Saisissez un mail \n");
@@ -38,7 +38,7 @@ void saisir_nouveau_client(FILE *rep)
         printf("Saisissez un decan (1er, 2ieme, 3ieme)\n");
         scanf("%s",p.decan);
 	    printf("Saisissez s'il y a un abonnement (Oui/Non)\n");
-        scanf("%s",p.abonnement);	
+        scanf("%s",p.abonnement);
         fprintf(rep,"\n%s %s %s %s", p.mail, p.signe, p.decan, p.abonnement);
     }
     else
@@ -52,9 +52,9 @@ int rechercher_client (FILE *rep, char *nomrech)
 {
 	int trouve=0;
 	CLIENT p;
-	
+
 	rep=fopen("bdd_client.txt","r");
-	
+
 	fseek(rep, 0, SEEK_SET);
 	while (fscanf(rep,"%s %s %s %s", p.mail, p.signe, p.decan, p.abonnement) !=EOF)
 		{
@@ -70,14 +70,14 @@ int rechercher_client (FILE *rep, char *nomrech)
 			}
 		}
 		fclose(rep);
-		return trouve;	
+		return trouve;
 }
 
 void modifier_client(FILE *rep, char *nomrech)
 {
 	int trouve=0;
     CLIENT p;
-    
+
 	trouve=rechercher_client(rep,nomrech);
 	rep=fopen("bdd_client.txt","r+");
 	if (trouve!=0)
@@ -86,12 +86,137 @@ void modifier_client(FILE *rep, char *nomrech)
 		printf("\nSaisir le nouvel abonnement (Oui/Non)\n");
 		scanf("%s", p.abonnement);
         fprintf(rep,"%s",p.abonnement);
-           
+
         listing_client(rep);
 	}
 	else
-	{	
+	{
 		printf("\nLa personne n existe pas");
 	}
 	fclose(rep);
+}
+
+void saisie_mot_cle(FILE *fic, REPONSE *p)
+{
+
+    fic=fopen("reponse.dat","ab");
+
+    if (fic==NULL)
+	{
+		printf("impossible d'ouvrir le fichier en lecture\n");
+		exit(EXIT_FAILURE); // equivalent a return EXIT_FAILURE;
+        /*fic=fopen("reponse.dat","w+b");
+		if (fic==NULL)
+		{
+			printf("impossible d'ouvrir le fichier en ecriture\n");
+			exit(EXIT_FAILURE); // equivalent a return EXIT_FAILURE;
+		}*/
+	}
+
+    if(fic != NULL)
+    {
+			printf("Saisissez un signe\n");
+			scanf("%s",p->signe);
+            fflush(stdin);
+            /* Remove trailing newline, if there. */
+            if ((strlen(p->signe) > 0) && (p->signe[strlen (p->signe) - 1] == '\n'))
+                p->signe[strlen (p->signe) - 1] = '\0';
+
+			printf("Saisissez un theme\n");
+			scanf("%s",p->theme);
+			if ((strlen(p->theme) > 0) && (p->theme[strlen (p->theme) - 1] == '\n'))
+                p->theme[strlen (p->theme) - 1] = '\0';
+
+			printf("Saisissez la reponse theme\n");
+			fgetc(stdin);
+			fgets(p->reponseTheme, sizeof(p->reponseTheme), stdin);
+            if ((strlen(p->reponseTheme) > 0) && (p->reponseTheme[strlen (p->reponseTheme) - 1] == '\n'))
+                p->reponseTheme[strlen (p->reponseTheme) - 1] = '\0';
+
+			printf("Saisissez un decan\n");
+			scanf("%s",p->decan);
+			if ((strlen(p->decan) > 0) && (p->decan[strlen (p->decan) - 1] == '\n'))
+                p->decan[strlen (p->decan) - 1] = '\0';
+
+			printf("Saisissez une reponse decan\n");
+			fgetc(stdin);
+			fgets(p->reponseDecan, sizeof(p->reponseDecan), stdin);
+            /* Remove trailing newline, if there. */
+
+			fwrite(p, 1 , sizeof(REPONSE) , fic );
+
+    }
+    else
+	{
+		printf("\nl ouverture du fichier c'est mal passe!");
+	}
+	fclose(fic);
+}
+
+void afficher_mot_cle(FILE *fic)
+{
+	REPONSE p;
+
+     fic=fopen("reponse.dat","rb");
+
+    if (fic==NULL)
+	{
+		printf("impossible d'ouvrir le fichier en lecture\n");
+		exit(EXIT_FAILURE); // equivalent a return EXIT_FAILURE;
+	}
+
+	/* Seek to the beginning of the file */
+	fseek(fic, 0, SEEK_SET);
+
+    if(fic != NULL)
+    {
+        while (fread(&p, sizeof(REPONSE), 1, fic))
+            printf("\nAffichage de la structure: signe=%s, theme=%s, reponse theme=%s, decan=%s,reponse decan=%s", p.signe, p.theme, p.reponseTheme, p.decan, p.reponseDecan);
+    }
+    else
+	{
+		printf("\nl ouverture du fichier c'est mal passe!");
+	}
+    fclose(fic);
+}
+
+void viderBuffer()
+{
+    int c = 0;
+    while (c != '\n' && c != EOF)
+    {
+        c = getchar();
+    }
+}
+
+/*
+    Exemple:
+    char nom[10];
+    printf("Quel est votre nom ? ");
+    lire(nom, 10);
+    printf("Ah ! Vous vous appelez donc %s !\n\n", nom);
+*/
+
+int lire(char *chaine, int longueur)
+{
+    char *positionEntree = NULL;
+
+    if (fgets(chaine, longueur, stdin) != NULL)
+    {
+        positionEntree = strchr(chaine, '\n');
+        if (positionEntree != NULL)
+        {
+            *positionEntree = '\0';
+        }
+        else
+        {
+            viderBuffer();
+        }
+        return 1;
+    }
+    else
+    {
+        viderBuffer();
+        return 0;
+    }
 }
--- src/main.c
+++ src/main.c
@@ -8,47 +8,47 @@
 int main(int argc, char *argv[])
 {
 	/* 0 => users ; 1 => admin */
-	int mode;
+	int mode = 0;
 	FILE *rep = NULL;
-	
-	//char *fileName = NULL;
-	//int nbDisplay = 1;
-	//int i;
-	
+	FILE *fic = NULL;
+
+    REPONSE p;
+
 	// parsing des arguments
-	if(argc > 3) 
+	if(argc > 3)
 	{
 		print_error( argv[0], " nombre d'arguments invalide" );
 		return EXIT_FAILURE;
 	}
 	// parsing des arguments
-	else if(argc < 2) 
+	else if(argc < 2)
 	{
 		mode = 0;
 	}
 	// parsing des arguments
 	else if (argc == 2)
 	{
-		if( strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "-help") == 0) 
+		if( strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "-help") == 0)
 		{
 			// affichage aide
 			print_help(argv[0]);
-			
+
 			// MACRO stdlib gestion exit
 			return EXIT_SUCCESS;
 		}
-		
+
 		else if( strcmp(argv[1], "-admin") == 0)
 		{
 			printf(" le password admin est manquant dans le second champs\n");
+			exit(EXIT_FAILURE); // equivalent a return EXIT_FAILURE;
 		}
-		else 
+		else
 		{
 			print_error( argv[0], " -h or -help for help menu or none if mode users is required" );
 			exit(EXIT_FAILURE); // equivalent a return EXIT_FAILURE;
 		}
 	}
-	else 
+	else
 	{
 		if( strcmp(argv[1], "-admin") == 0)
 		{
@@ -69,25 +69,28 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	switch(mode) 
+	switch(mode)
 	{
 		case 0:
 			menu_utilisateur();
 			//printf(" ici appel du menu mode users\n");
 		break;
 		case 1:
-			menu_admin(rep);
+			menu_admin(rep, fic, &p);
 			//printf(" ici appel du menu mode admin\n");
 		break;
+		default:
+            exit(EXIT_FAILURE); // equivalent a return EXIT_FAILURE;
+        break;
 	}
-	
+
 
 	return EXIT_SUCCESS;
 }
 
 // Affiche l'aide du programme
-void print_help(char *prgName) 
-{	
+void print_help(char *prgName)
+{
 	printf("\n Show program help \n\n");
 	printf(" %s <opt> <password> \n\n", prgName);
 	printf(" <opt> is either '-h' or '-help' or '-admin'.\n\n");
@@ -95,7 +98,7 @@ void print_help(char *prgName)
 }
 
 // Affiche l'erreur errStr, puis l'aide du programme
-void print_error(char *prgName, char *errStr) 
+void print_error(char *prgName, const char *errStr)
 {
 	printf(" Error : '%s'\n", errStr);
 	print_help(prgName);
@@ -104,9 +107,9 @@ void print_error(char *prgName, char *errStr)
 void menu_utilisateur()
 {
 	int choix = 0;
-	
+
 	//rep=fopen("bdd_client.txt","a+");
-	
+
 	do
 	{
 		do
@@ -118,25 +121,25 @@ void menu_utilisateur()
 			printf("Votre choix : ");
 			scanf("%d", &choix);
 		}while (choix < 1 || choix > 3);
-		
+
 		switch (choix)
 		{
 			case 1 : printf(" ici appel de la fonction saisie mail\n");//saisie_mail(fichier);
 				 break;
 			case 2 : printf(" Aujourd'hui super promo pour deux themes Astral achete un theme astral offert\n");
 				 break;
-				 
+
 			default : break;
 		}
 	}while (choix != 3);
 }
 
-void menu_admin(FILE *rep)
+void menu_admin(FILE *rep, FILE *fic, REPONSE *p)
 {
 	int choix = 0;
 	int recherche = 0;
 	char nomrech[100];
-	
+
 	do
 	{
 		do
@@ -145,7 +148,7 @@ void menu_admin(FILE *rep)
 			printf("1-Ajouter un client dans la bdd\n");
 			printf("2-Afficher la bdd client\n");
 			printf("3-Rechercher un client dans la bdd\n");
-			printf("4-Modifier m'l'abonnement d'un client dans la bdd\n");
+			printf("4-Modifier l'abonnement d'un client dans la bdd\n");
 			printf("5-Ajouter un mot cle et sa reponse dans la bdd mot cle\n");
 			printf("6-Afficher les mots cles et leurs reponses\n");
 			printf("7-Rechercher un mot cle et sa reponse\n");
@@ -154,36 +157,38 @@ void menu_admin(FILE *rep)
 			printf("Votre choix : ");
 			scanf("%d", &choix);
 		}while (choix < 1 || choix > 9);
-		
+
 		switch (choix)
 		{
 			case 1 : 	saisir_nouveau_client(rep);
 				 break;
-				 
+
 			case 2 : 	listing_client(rep);
 				 break;
-				 
+
 			case 3 :	printf("Saisissez un nom a rechercher\n");
-						scanf("%s",nomrech); 
+						scanf("%s",nomrech);
 						recherche=rechercher_client (rep, nomrech);
 						//memset effacer le nom saisie?
 				 break;
-				 
+
 			case 4 : 	printf("Saisissez un nom sur laquelle vous voulez changer l'abonnement\n");
 						scanf("%s",nomrech);
 						modifier_client(rep, nomrech);
 						//memset effacer le nom saisie?
 				 break;
-				 
-			case 5 : printf(" ici appel de la fonction Ajouter un mot clé et sa reponse dans la bdd mot clé\n");
+
+			case 5 :    printf("Ajouter un mot cle et sa reponse dans la bdd mot cle\n");
+                       		    saisie_mot_cle(fic, p);
 				 break;
-			case 6 : printf(" ici appel de la fonction Afficher les mots clés et leurs reponses\n");//saisie_mail(fichier);
+			case 6 :    printf("Afficher les mots cles et leurs reponses\n");
+                       		    afficher_mot_cle(fic);
 				 break;
-			case 7 : printf(" ici appel de la fonction Rechercher un mot clé et sa reponse\n");
+			case 7 : printf(" ici appel de la fonction Rechercher un mot cle et sa reponse\n");
 				 break;
-			case 8 : printf(" ici appel de la fonction Supprimer un mot clé et sa reponse\n");//saisie_mail(fichier);
+			case 8 : printf(" ici appel de la fonction Supprimer un mot cle et sa reponse\n");
 				 break;
-				 
+
 			default : break;
 		}
 	}while (choix != 9);
