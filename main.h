6bd76d45a3db2172116a2969f5bb9c4a2c6b5daf
fred-isen <74205795+fred-isen@users.noreply.github.com>
2020-12-10 23:35:15 +0100

    Add files via upload

---
+++ main.h
@@ -0,0 +1,23 @@
+/*Fichier main.h ==> prototypes et structures */
+
+typedef struct{
+    char adress_EM[50];
+	char adress_DEST[50];
+    char OBJ[100];
+	char CORPS[1500];
+}EMAIL;
+
+typedef struct
+{
+    char signe[20];
+    char theme[20];
+    char reponseTheme[1500];
+    char decan[20];
+    char reponseDecan[1500];
+}REPONSE;
+
+void print_help(char * prgName);
+void print_error(char * prgName, const char * errStr);
+void menu_utilisateur(FILE *rep, EMAIL *q);
+void menu_admin(FILE *rep, FILE *fic, FILE *mot, REPONSE *p);
+
